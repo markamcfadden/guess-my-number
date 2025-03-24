4e9bd8e14b4350f89036ad875a2382922b377bec
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-24 14:23:17 -0300

    add styling changes when correct number guessed

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const secretNumber = Math.floor(Math.random() * 100) + 1;
+let secretNumber = Math.floor(Math.random() * 1000) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".number").textContent = secretNumber;
 
@@ -10,8 +11,16 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number guessed!";
+    //winning guess
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+    //wrong guess
   } else if (guess > secretNumber || guess < secretNumber) {
     score--;
     document.querySelector(".score").textContent = score;
@@ -25,4 +34,14 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "Game over!";
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.floor(Math.random() * 1000) + 1;
+  document.querySelector(".number").textContent = secretNumber;
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
